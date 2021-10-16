4bf7f70a184c1231ce4c141ed7c2f5db044d969c
Rafael Rivera <a20135321@pucp.edu.pe>
2021-10-16 08:21:38 -0500

    FIRST COMMIT

---
+++ TestModel/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestModel
+{
+    public class Product
+    {
+        public int id;
+        public string code;
+        public float weight;
+        public float price;
+
+        public Product(int id, string code, float weight, float price)
+        {
+            this.id = id;
+            this.code = code;
+            this.weight = weight;
+            this.price = price;
+        }
+    }
+}
---
+++ TestModel/dbConnection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace TestModel
+{
+    public class DbConnection
+    {       
+        private SqlConnection sqlConnection;
+
+        public DbConnection(){
+            this.sqlConnection = new SqlConnection();
+        }
+
+        public void OpenConnection()
+        {
+            this.sqlConnection.ConnectionString = "Data Source=LAPTOP-GTUUG201\\SQLEXPRESS;Initial Catalog=TestForms;Integrated Security=True";
+            this.sqlConnection.Open();
+        }
+
+        public void CloseConnection()
+        {
+            this.sqlConnection.Close();
+        }
+
+        public SqlCommand Query(string queryString)
+        {
+            SqlCommand query = new SqlCommand
+            {
+                Connection = this.sqlConnection,
+                CommandText = queryString
+            };
+            return query;
+        }
+
+    }
+}
